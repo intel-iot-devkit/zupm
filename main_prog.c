531616afcab260b75c2ec63c5ebab77cbfc988a0
Abhishek Malik <abhishek.malik@intel.com>
2016-05-17 16:52:52 -0700

    Servo and DL Open: Initial Implementation
    This commit contains 2 drivers, es08a and es9257. Both of these
    are servo motors. The file main_prog.c is the main file and
    instructions to run this code are in the file itself. It uses
    dlopen and an include for upm_servo.h would be required.
    
    Signed-off-by: Abhishek Malik <abhishek.malik@intel.com>

---
+++ main_prog.c
@@ -0,0 +1,35 @@
+// To run:
+// build the 2 drivers in the folder as a part of UPM
+// and update edison with this built version of UPM
+// copy over this file to the edison
+// set library path as: LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/lib
+// compile as: gcc main_prog.c -ldl -I/usr/include/upm
+// run as: ./a.out
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <dlfcn.h>
+#include "upm_servo.h"
+
+int main(int argc, char **argv) {
+	void *handle;
+	struct _upm_servo_ft (*getft)();
+	char *error;
+
+	handle = dlopen("/usr/lib/libupm-upm_c_es08a.so", RTLD_LAZY);
+	if(!handle){
+		printf("Error in fetching handle");
+		exit(1);
+	}
+
+	getft = dlsym(handle, "upm_servo_get_function_table");
+	if((error =dlerror()) != NULL){
+		fputs(error, stderr);
+		exit(1);
+	}
+
+	struct _upm_servo_ft ft = (*getft)();
+	void* dev = ft.upm_servo_init(3,3,600,2200);
+
+	dlclose(handle);
+}
---
+++ upm_c_es9257/es9257.h
@@ -0,0 +1,117 @@
+/*
+ * es9257.h
+ *
+ *  Created on: May 6, 2016
+ *  Modified: Abhishek Malik <abhishek.malik@intel.com>
+ */
+
+#ifndef ES9257_H_
+#define ES9257_H_
+
+#pragma once
+
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <mraa/pwm.h>
+#include "../upm_servo.h"
+
+/**
+ * @library servo
+ * @sensor es9257
+ * @comname ES9257 Servo
+ * @altname Grove Servo
+ * @type servos
+ * @man emax
+ * @web http://www.seeedstudio.com/wiki/Grove_-_Servo
+ * @con pwm
+ * @kit gsk
+ *
+ * @brief API for the ES9257 Servo
+ *
+ * This module defines the ES9257 interface for ES9257 servos.
+ * Like other servos, the ES9257 servo has a shaft that can be controlled
+ * by setting the desired angle. There are also routines for setting
+ * and getting the minimum and maximum pulse width as well as the
+ * maximum period.
+ *
+ * @image html es9257.jpg
+ * @snippet es9257.c Interesting
+ */
+
+#define UPM_ES9257_MIN_PULSE_WIDTH  475
+#define UPM_ES9257_MAX_PULSE_WIDTH  2100
+#define UPM_ES9257_PERIOD           20000
+#define UPM_ES9257_MAX_ANGLE		180.0
+
+#define UPM_ES9257_HIGH             1
+#define UPM_ES9257_LOW              0
+
+/*
+ * Opaque pointer to the servo motor struct
+ */
+//struct _upm_es9257;
+typedef struct _upm_es9257* UpmES9257;
+
+/**
+ * Instantiates a the servo at the given pin
+ *
+ * @param pin Servo pin number
+ * @param minPulseWidth Minimum pulse width, in microseconds
+ * @param maxPulseWidth Maximum pulse width, in microseconds
+ * @param waitAndDisablePwm If 1, PWM is enabled only during the setAngle() execution
+ * for a period of 1 second, and then turned back off. If 0, PWM remains on afterward.
+ */
+//upm_es9257 upm_es9257_init(int32_t pin, int32_t min_pulse_width, int32_t max_pulse_width);
+//void* upm_es9257_init(int32_t pin, int32_t min_pulse_width, int32_t max_pulse_width);
+void* upm_es9257_init(int num,...);
+
+/**
+ * Halts PWM for this servo and allows it to move freely.
+ */
+mraa_result_t upm_es9257_halt(void* dev);
+
+/**
+ * Sets the angle of the servo engine.
+ *
+ * @param angle Number between 0 and 180
+ * @return 0 if successful, non-zero otherwise
+ */
+mraa_result_t upm_es9257_set_angle(void* dev, int32_t angle);
+
+/*
+ * Calculating relative pulse time to the value.
+ * */
+int32_t upm_es9257_calc_pulse_travelling(void* dev, int32_t value);
+
+/**
+ * Sets the minimum pulse width
+ *
+ * @param width Minimum HIGH signal width
+ */
+void upm_es9257_set_min_pulse_width (void* dev, int width);
+
+/**
+ * Sets the maximum pulse width
+ *
+ * @param width Maximum HIGH signal width
+ */
+void upm_es9257_set_max_pulse_width (void* dev, int width);
+
+/**
+ * Returns the minimum pulse width
+ *
+ * @return Minimum pulse width
+ */
+int upm_es9257_get_min_pulse_width (void* dev);
+
+/**
+ * Returns the maximum pulse width
+ *
+ * @return Maximum pulse width
+ */
+int upm_es9257_get_max_pulse_width (void* dev);
+
+void UpmES9257GetSensorInfo();
+
+#endif /* ES9257_H_ */
