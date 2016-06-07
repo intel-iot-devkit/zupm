f2afcf1137a164f72f52be977a30784e74137dd7
Mihai Tudor Panu <mihai.tudor.panu@intel.com>
2016-06-07 10:31:46 -0700

    upm_types: added more error types and sensor categories
    Signed-off-by: Mihai Tudor Panu <mihai.tudor.panu@intel.com>

--- include/upm_types.h
+++ include/upm_types.h
@@ -34,30 +34,48 @@ typedef enum {
 #endif
 
 typedef enum {
-	UPM_SUCCESS,						/* Operation is successful */
-	UPM_MODE_NOT_SUPPORTED,				/* Trying to access one mode while working on another mode */
-	UPM_ERROR_UNSPECIFIED,				/* Unspecified error */
-	UPM_ERROR_OUT_OF_RANGE,				/* When the input to drive is too high/low or -ve */
-	UPM_ERROR_OPERATION_FAILED,			/* When a function isn't able to perform as expected */
-	UPM_ERROR_TIMED_OUT,
-	UPM_ERROR_INVALID_PACKET_SIZE
+	UPM_SUCCESS = 0,						/* Operation is successful, expected response */
+    UPM_ERROR_NOT_IMPLEMENTED = 1,		    /* Trying to access a feature or mode that is not implemented */
+	UPM_ERROR_NOT_SUPPORTED = 2,			/* Trying to access a feature or mode that is not supported */
+    UPM_ERROR_NO_RESOURCES = 3,             /* No resources to perform operation */
+	UPM_ERROR_NO_DATA = 4,                  /* No data received or available from the sensor */
+    UPM_ERROR_INVALID_PARAMETER = 5,        /* Invalid parameter passed to the function*/
+	UPM_ERROR_INVALID_SIZE = 6,             /* Invalid buffer size */
+    UPM_ERROR_OUT_OF_RANGE = 7,				/* When the input to drive is too high/low or -ve */
+	UPM_ERROR_OPERATION_FAILED = 8,			/* When a function isn't able to perform as expected */
+	UPM_ERROR_TIMED_OUT = 9,                /* Timed out while communicating with the sensor */
+    
+    UPM_ERROR_UNSPECIFIED = 99				/* Unspecified/Unknown error */
 } upm_result_t;
 
 /* Sensor categories */
 typedef enum {
     UPM_ACCELEROMETER,
+    UPM_ANGLE,
+    UPM_AUDIO,
     UPM_COMPASS,
+    UPM_CURRENT,
+    UPM_DISPLAY,
     UPM_DISTANCE,
+    UPM_FLOW,
+    UPM_FORCE,
+    UPM_GAS,
     UPM_GYROSCOPE,
     UPM_HEART_RATE,
+    UPM_HUMIDITY,
     UPM_IMU,
     UPM_LIGHT,
     UPM_PH,
+    UPM_POTENTIOMETER,
     UPM_PRESSURE,
     UPM_SERVO,
     UPM_STEPPER,
+    UPM_SWITCH,
     UPM_TEMPERATURE,
-    UPM_TOUCH
+    UPM_TIME,
+    UPM_VIDEO,
+    UPM_VOLTAGE,
+    UPM_WIRELESS
 } upm_sensor_t;
 
 /* Supported IO protocols via MRAA */
@@ -75,6 +93,8 @@ typedef enum {
 typedef struct _upm_sensor_descriptor {
     char* name;
     char* description;
+    int protocol_size;
+    upm_protocol_t* protocol;
     int category_size;
     upm_sensor_t* category;
 } upm_sensor_descriptor_t;
