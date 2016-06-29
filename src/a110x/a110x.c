e9fc8d8928362c9d886c00e98f794e65bc2e2a67
Abhishek Malik <abhishek.malik@intel.com>
2016-06-29 14:42:51 -0700

    A110X: Initial Commit - Hall Sensor
    Driver for the Grove Hall Sensor. Tested on Edison.
    
    Signed-off-by: Abhishek Malik <abhishek.malik@intel.com>

---
+++ src/a110x/a110x.c
@@ -0,0 +1,128 @@
+/*
+ * Author: Jon Trulson <jtrulson@ics.com>
+ * 		   Abhishek Malik <abhishek.malik@intel.com>
+ * Copyright (c) 2016 Intel Corporation.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+ * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+#include "a110x.h"
+
+struct _upm_a110x {
+	mraa_gpio_context		gpio;
+	uint8_t					gpio_pin;
+	bool					isr_installed;
+};
+
+#if defined(CONFIG_BOARD_ARDUINO_101) || defined(CONFIG_BOARD_ARDUINO_101_SSS) || defined(CONFIG_BOARD_QUARK_D2000_CRB)
+DEFINE_MEM_MAP(UPM_A110X_MEM_MAP, 1, sizeof(struct _upm_a110x));
+const kmemory_map_t UPM_A110X_MEM_MAP;
+#elif defined(linux)
+#define UPM_A110X_MEM_MAP 0
+#endif
+
+const char upm_a110x_name[] = "A110X";
+const char upm_a110x_description[] = "A110X Hall Effect Sensor";
+const upm_protocol_t upm_a110x_protocol[] = {UPM_GPIO};
+const upm_sensor_t upm_a110x_category[] = {UPM_ELECTRICITY};
+
+void* upm_a110x_get_ft(upm_sensor_t sensor_type){
+	if(sensor_type == UPM_SENSOR){
+		struct _upm_sensor_ft *ft = malloc(sizeof(*ft));
+		if(ft == NULL){
+			printf("Unable to assign memory to the Hall Effect Sensor structure");
+			return NULL;
+		}
+		ft->upm_sensor_init_name = upm_a110x_init_name;
+		ft->upm_sensor_close = upm_a110x_close;
+		ft->upm_sensor_read = upm_a110x_read;
+		ft->upm_sensor_write = upm_a110x_write;
+		return ft;
+	}
+	return NULL;
+}
+
+upm_sensor_descriptor_t upm_a110x_get_descriptor(void* dev){
+	upm_sensor_descriptor_t usd;
+	usd.name = upm_a110x_name;
+	usd.description = upm_a110x_description;
+	usd.protocol_size = 1;
+	usd.protocol = upm_a110x_protocol;
+	usd.category_size = 1;
+	usd.category = upm_a110x_category;
+	return usd;
+}
+
+void* upm_a110x_init_name(int num,...){
+	return NULL;
+}
+
+void* upm_a110x_init(uint8_t pin){
+	upm_a110x dev = (upm_a110x) upm_malloc(UPM_A110X_MEM_MAP, sizeof(struct _upm_a110x));
+	dev->gpio_pin = pin;
+	dev->gpio = mraa_gpio_init(dev->gpio_pin);
+	if(mraa_gpio_dir(dev->gpio, MRAA_GPIO_IN) != MRAA_SUCCESS){
+		return NULL;
+	}
+	dev->isr_installed = false;
+	return dev;
+}
+
+void upm_a110x_close(void* dev){
+	upm_free(UPM_A110X_MEM_MAP, dev);
+}
+
+upm_result_t upm_a110x_read(void* dev, void* value, int* len){
+	upm_a110x device = (upm_a110x) dev;
+	int* int_val = value;
+	*int_val = mraa_gpio_read(device->gpio);
+	return UPM_SUCCESS;
+}
+
+upm_result_t upm_a110x_write(void* dev, void* value, int len){
+	return UPM_ERROR_NOT_IMPLEMENTED;
+}
+
+upm_result_t upm_a110x_magnet_detected(void* dev, bool* res){
+	upm_a110x device = (upm_a110x) dev;
+	int val, len;
+	upm_a110x_read(device, &val, &len);
+	if(val == 0)
+		*res = false;
+	else
+		*res = true;
+	return UPM_SUCCESS;
+}
+
+upm_result_t upm_a110x_install_isr(void* dev, mraa_gpio_edge_t edge_level, void (*isr)(void *), void *arg){
+	upm_a110x device = (upm_a110x) dev;
+	if(device->isr_installed)
+		upm_a110x_uninstall_isr(device);
+
+	mraa_gpio_isr(device->gpio, edge_level, isr, arg);
+	device->isr_installed = true;
+	return UPM_SUCCESS;
+}
+
+upm_result_t upm_a110x_uninstall_isr(void* dev){
+	upm_a110x device = (upm_a110x) dev;
+	mraa_gpio_isr_exit(device->gpio);
+	device->isr_installed = false;
+	return UPM_SUCCESS;
+}
---
+++ src/a110x/a110x.h
@@ -0,0 +1,137 @@
+/*
+ * Author: Jon Trulson <jtrulson@ics.com>
+ * 		   Abhishek Malik <abhishek.malik@intel.com>
+ * Copyright (c) 2016 Intel Corporation.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+ * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+#ifndef A110X_A110X_H_
+#define A110X_A110X_H_
+#include "../upm.h"
+/**
+ * @brief A110X Hall Effect library
+ * @defgroup a110x libupm-a110x
+ * @ingroup gpio electric robok
+ */
+
+/**
+ * @library a110x
+ * @sensor a110x
+ * @comname A110X Hall Effect Sensor
+ * @altname Grove Hall Sensor
+ * @altid A1101, A1102, A1103, A1004, A1106
+ * @type electric
+ * @web http://www.allegromicro.com/en/Products/Magnetic-Digital-Position-Sensor-ICs/Hall-Effect-Unipolar-Switches/A1101-2-3-4-6.aspx
+ * @con gpio
+ * @kit robok
+ *
+ * @brief API for the A110X Hall Effect sensors
+ *
+ * UPM module for the A110X (A1101, A1102, A1103, A1104, and A1106)
+ * Hall Effect sensors.  It outputs a digital signal indicating
+ * whether it is detecting a magnetic field with south polarity
+ * perpendicular to the sensor element.
+ *
+ * @image html a110x.jpg
+ * An example showing a simple test for the presence of a field
+ * @snippet a110x.cxx Interesting
+ * An example demonstrating the use of an interrupt handler to count pulses
+ * @snippet a110x-intr.cxx Interesting
+ */
+
+/**
+ * Opaque pointer to the sensor structure
+ */
+typedef struct _upm_a110x* upm_a110x;
+
+/**
+ * A110X Name Initialization function
+ *
+ * @return void* pointer to the sensor struct
+ */
+void* upm_a110x_init_name(int num,...);
+
+/**
+ * A110X Initialization function
+ *
+ * @param pin GPIO pin to use
+ * @return void* pointer to the sensor struct
+ */
+void* upm_a110x_init(uint8_t pin);
+
+/**
+ * A110X Initialization function
+ *
+ * @param void* pointer to the sensor struct
+ */
+void upm_a110x_close(void* dev);
+
+/**
+ * Determines whether a magnetic field of south polarity has been detected
+ *
+ * @param void* pointer to the sensor struct
+ * @param bool* to note the response
+ * @return upm_result_t UPM success/error code
+ */
+upm_result_t upm_a110x_magnet_detected(void* dev, bool* res);
+
+/**
+ * Installs an interrupt service routine (ISR) to be called when
+ * the appropriate magnetic field is detected
+ *
+ * @param void* pointer to the sensor struct
+ * @param mraa_gpio_edge_t edge trigger level
+ * @param isr ISR callback function
+ * @return upm_result_t UPM success/error code
+ */
+upm_result_t upm_a110x_install_isr(void* dev, mraa_gpio_edge_t edge_level, void (*isr)(void *), void *arg);
+
+/**
+ * Uninstalls the previously installed ISR
+ *
+ * @param void* pointer to the sensor struct
+ * @return upm_result_t UPM success/error code
+ */
+upm_result_t upm_a110x_uninstall_isr(void* dev);
+
+/**
+ * Generic read function for the sensor. Returns
+ * raw value.
+ *
+ * @param void* pointer to the sensor struct
+ * @param void* value stores the value that was read
+ * @param int len length of the elements of the
+ * value that has been read
+ * @return upm_result_t UPM success/error code
+ */
+upm_result_t upm_a110x_read(void* dev, void* value, int* len);
+
+/**
+ * Generic write function for the sensor.
+ *
+ * @param void* pointer to the sensor struct
+ * @param void* value stores the value to write
+ * @param int len length of the elements of the
+ * value to be written
+ * @return upm_result_t UPM success/error code
+ */
+upm_result_t upm_a110x_write(void* dev, void* value, int len);
+
+#endif /* A110X_A110X_H_ */
