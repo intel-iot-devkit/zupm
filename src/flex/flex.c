c086b717dc7ee6881a355eac7cfaea5754dcf047
Jon Trulson <jtrulson@ics.com>
2016-08-09 16:44:45 -0600

    flex: make the device context public
    Signed-off-by: Jon Trulson <jtrulson@ics.com>

--- src/flex/flex.c
+++ src/flex/flex.c
@@ -26,21 +26,6 @@
 #include <stdlib.h>
 
 #include "flex.h"
-#include "mraa/aio.h"
-
-/**
- * Analog sensor struct
- */
-typedef struct _flex_context {
-    /* mraa aio pin context */
-    mraa_aio_context aio;
-    /* Analog voltage reference */
-    float m_aRef;
-    /* Raw count offset */
-    float m_count_offset;
-    /* Raw count scale */
-    float m_count_scale;
-} *flex_context;
 
 flex_context flex_init(int16_t pin)
 {
