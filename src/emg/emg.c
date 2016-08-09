4a614810e997e68fd0fde757fdf305bddc863430
Jon Trulson <jtrulson@ics.com>
2016-08-09 16:39:01 -0600

    emg: make the device context public
    Signed-off-by: Jon Trulson <jtrulson@ics.com>

--- src/emg/emg.c
+++ src/emg/emg.c
@@ -26,21 +26,6 @@
 #include <stdlib.h>
 
 #include "emg.h"
-#include "mraa/aio.h"
-
-/**
- * Analog sensor struct
- */
-typedef struct _emg_context {
-    /* mraa aio pin context */
-    mraa_aio_context aio;
-    /* Analog voltage reference */
-    float m_aRef;
-    /* Raw count offset */
-    float m_count_offset;
-    /* Raw count scale */
-    float m_count_scale;
-} *emg_context;
 
 emg_context emg_init(int16_t pin)
 {
--- src/emg/emg.h
+++ src/emg/emg.h
@@ -25,15 +25,25 @@
 #pragma once
 
 #include "upm.h"
+#include "mraa/aio.h"
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
 /**
- * Opaque pointer to sensor structure
+ * device context
  */
-typedef struct _emg_context *emg_context;
+typedef struct _emg_context {
+    /* mraa aio pin context */
+    mraa_aio_context aio;
+    /* Analog voltage reference */
+    float m_aRef;
+    /* Raw count offset */
+    float m_count_offset;
+    /* Raw count scale */
+    float m_count_scale;
+} *emg_context;
 
 /**
  * Initialize analog sensor
