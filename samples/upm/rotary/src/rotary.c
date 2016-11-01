7bd85e3df945a5c9fd40f6446c34cd03a14d5ad6
Abhishek Malik <abhishek.malik@intel.com>
2016-11-01 14:42:55 -0700

    Rotary: Adding C example
    Signed-off-by: Abhishek Malik <abhishek.malik@intel.com>

---
+++ samples/upm/rotary/src/rotary.c
@@ -0,0 +1,56 @@
+/*
+ * Author: Jon Trulson <jtrulson@ics.com>
+ * Copyright (c) 2015 Intel Corporation.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+ * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "rotary.h"
+#include "upm_utilities.h"
+#include "mraa.h"
+
+int main()
+{
+    if (mraa_init() != MRAA_SUCCESS)
+    {
+        perror("Failed to initialize mraa\n");
+        return -1;
+    }
+
+//! [Interesting]
+    // Instantiate a Grove Rotary Angle Sensor, using analog pins A0
+    rotary_context dev = rotary_init(0, 5.0);
+
+    float angle;
+    while(1) {
+        rotary_get_value_voltage(dev, &angle);
+        printf("rotary voltage value detected: %f\n", angle);
+        upm_delay(1);
+    }
+
+    rotary_close(dev);
+//! [Interesting]
+    return 0;
+}
