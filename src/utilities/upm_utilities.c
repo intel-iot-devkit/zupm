d32dd4839c187c6213f6987fde6b485c9551bf4d
Noel Eck <noel.eck@intel.com>
2016-11-30 12:38:53 -0800

    timers: Replaced depreciated nano/micro_timer_ methods
        * New methods: k_timer_init, k_timer_start, k_timer_status_sync
    
    Signed-off-by: Noel Eck <noel.eck@intel.com>

--- src/utilities/upm_utilities.c
+++ src/utilities/upm_utilities.c
@@ -30,11 +30,10 @@ void upm_delay(int time){
 #if defined(UPM_PLATFORM_LINUX)
     sleep(time);
 #elif defined(UPM_PLATFORM_ZEPHYR)
-    struct nano_timer timer;
-    void *timer_data[1];
-    nano_timer_init(&timer, timer_data);
-    nano_timer_start(&timer, SECONDS(time) + 1);
-    nano_timer_test(&timer, TICKS_UNLIMITED);
+    struct k_timer timer;
+    k_timer_init(&timer, NULL, NULL);
+    k_timer_start(&timer, SECONDS(time) + 1, 0);
+    k_timer_status_sync(&timer);
 #endif
 }
 
@@ -42,11 +41,10 @@ void upm_delay_ms(int time){
 #if defined(UPM_PLATFORM_LINUX)
     usleep(1000 * time);
 #elif defined(UPM_PLATFORM_ZEPHYR)
-    struct nano_timer timer;
-    void *timer_data[1];
-    nano_timer_init(&timer, timer_data);
-    nano_timer_start(&timer, MSEC(time) + 1);
-    nano_timer_test(&timer, TICKS_UNLIMITED);
+    struct k_timer timer;
+    k_timer_init(&timer, NULL, NULL);
+    k_timer_start(&timer, MSEC(time) + 1, 0);
+    k_timer_status_sync(&timer);
 #endif
 }
 
@@ -54,11 +52,10 @@ void upm_delay_us(int time){
 #if defined(UPM_PLATFORM_LINUX)
     usleep(time);
 #elif defined(UPM_PLATFORM_ZEPHYR)
-    struct nano_timer timer;
-    void *timer_data[1];
-    nano_timer_init(&timer, timer_data);
-    nano_timer_start(&timer, USEC(time) + 1);
-    nano_timer_test(&timer, TICKS_UNLIMITED);
+    struct k_timer timer;
+    k_timer_init(&timer, NULL, NULL);
+    k_timer_start(&timer, USEC(time) + 1, 0);
+    k_timer_status_sync(&timer);
 #endif
 }
 
