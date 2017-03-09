b83e2a2b7d42d290884ec61ac85ee380ebfc263b
Abhishek Malik <abhishek.malik@intel.com>
2017-03-08 18:17:54 -0800

    tsl2561: Updated sample to work on d2000
    A few small changes to sample source.  Works on d2000 and arduino_101
    
    Signed-off-by: Abhishek Malik <abhishek.malik@intel.com>

--- samples/upm/tsl2561/src/tsl2561.c
+++ samples/upm/tsl2561/src/tsl2561.c
@@ -19,11 +19,28 @@ int main()
     }
 
     tsl2561_context dev = tsl2561_init(0, TSL2561_Address, GAIN_0X, INTEGRATION_TIME1_101MS);
-    float abc = 0;
-    if(tsl2561_get_lux(dev, &abc) != UPM_SUCCESS){
-        printf("ERROR !! ERROR !! ERROR!!");
+    if (!dev)
+    {
+        fprintf(stderr,"Failed to initialize digital light sensor\n");
+        return -1;
     }
-    printf("value retrieved: %f\n", abc);
+
+    float abc = 0;
+    while (1)
+    {
+        if(tsl2561_get_lux(dev, &abc) != UPM_SUCCESS){
+            fprintf(stderr, "ERROR !! ERROR !! ERROR!!");
+            return -1;
+        }
+
+#if defined(CONFIG_BOARD_QUARK_D2000_CRB)
+        int abc_int = abc;
+        printf("value retrieved: %d\n", abc_int);
+#else
+        printf("value retrieved: %f\n", abc);
+#endif
+        upm_delay_ms(500);
+    }
 
     return 0;
 }
